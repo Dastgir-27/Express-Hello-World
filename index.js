c3cc0d2f112a6f0e5ce3f6f64271f6ff6c3d3bff
Dastgir-27 <dastgiridrisi27@gmail.com>
2025-04-19 19:38:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(8080, function () {
-  console.log('Listening on port 8080...')
+app.listen(3000, function () {
+  console.log('Listening on port 3000...')
 })
